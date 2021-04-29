b856d7b645a7d25f3898bd229846dfbcf10e92c0
28 <kevinrody163@gmail.com>
2021-04-29 15:01:11 +0400

    '用户登录注册和个人中心'

--- userapp/views.py
+++ userapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views import View
 from userapp.models import *
 from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
+from utils.code import *
+from django.core.serializers import serialize
 
 
 # Create your views here.
@@ -16,11 +18,13 @@ class RegisterView(View):
         uname = request.POST.get('uname')
         pwd = request.POST.get('pwd')
         # 插入数据库
-        user = userInfo.objects.create(uname=uname, pwd=pwd)
+        user = UserInfo.objects.create(uname=uname, pwd=pwd)
+        print('user:', user)
         # 判断是否注册成功
         if user:
             # 将用户信息存放值session对象中，每一个模块都共享session：全局上下文
             request.session['user'] = user
+            print('request.session["user"]:', request.session['user'])
             return HttpResponseRedirect('/user/center/')
 
         return HttpResponseRedirect('/user/register/')
@@ -32,7 +36,7 @@ class checkUnameView(View):
         # 获取请求参数后去数据库查询
         uname = request.GET.get('uname', '')
         # 根据用户名去数据库查询 如果使用get，数据不存在时会报错
-        userList = userInfo.objects.filter(uname=uname)
+        userList = UserInfo.objects.filter(uname=uname)
         # 判断userList是否为空
         # 定义变量
         flag = False
@@ -44,4 +48,86 @@ class checkUnameView(View):
 
 class CenterView(View):
     def get(self, request):
-        return render(request, 'center.html')
+        # 个人中心页面判断用户是否登录，判断session是否存在
+        if 'user' in request.session:
+            return render(request, 'center.html')
+        return render(request, 'login.html')
+
+
+class LogoutView(View):
+    def post(self, request):
+        # 退出删除session中的用户登录信息
+        if 'user' in request.session:
+            del request.session['user']
+
+            return HttpResponse({'flag': True})
+
+
+class LoginView(View):
+    def get(self, request):
+        # 判断用户是否已经登录过
+        if 'user' in request.session:
+            return render(request, 'center.html')
+        return render(request, 'login.html')
+
+    def post(self, request):
+        # 获取请求参数
+        uname = request.POST.get('uname', '')
+        print('uname:', uname)
+        pwd = request.POST.get('pwd', '')
+        print('pwd:', pwd)
+        # 查询userInfo表数据是否存在
+        # filter对象返回的为一个列表
+        userList = UserInfo.objects.filter(uname=uname, pwd=pwd)
+        print('userList', userList)
+        if userList:
+            # 登录的用户只能有一个
+            print('user', userList[0])
+            request.session['user'] = userList[0]
+            return HttpResponseRedirect('/user/center/')
+        return HttpResponseRedirect('/user/login/')
+
+
+# 引用验证码工具，返回二维码图片
+class LoadCodeView(View):
+    def get(self, request):
+        # 需要导入工具包
+        # 返回一个元祖 一个图片，一个字符串，元祖为()
+        img, str = gene_code()
+        # 将生成的验证码放入session中
+        request.session['sessionCode'] = str
+        # 指定图片格式在code中指定了png 所有这里为png 写法参考mime类型搜索image
+        return HttpResponse(img, content_type='image/png')
+
+
+# 登录时局部验证二维码
+class CheckCodeView(View):
+    def get(self, request):
+        # 需要获取输入框的值，在ajax请求中key为code
+        code = request.GET.get('code', '')
+        # 获取生成的验证码进行比较 生成的验证码存储与session
+        session_code = request.session.get('sessionCode', None)
+        # 比较是否相等，不适用if写法
+        flag = code == session_code
+        return JsonResponse({'checkFlag': flag})
+
+
+class AddressView(View):
+    def get(self, request):
+        return render(request, 'address.html')
+
+
+class LoadAreaView(View):
+    # 获取请求参数
+    def get(self, request):
+        pid = request.GET.get('pid', -1)
+        pid = int(pid)
+        print('pid', pid)
+        # 根据父id查询区划信息 父id和他上一级id是相等的，areaLevel1为最上级父id，id为0
+        areaList = Area.objects.filter(parentid=pid)
+        print('areaList:', areaList)
+        # 需要系列化
+        jAreaList = serialize('json', areaList)
+        print('jAreaList:', jAreaList)
+
+        return JsonResponse({'jAreaList': jAreaList})
